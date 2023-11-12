d38909717d7d9ed5f299d3a26475e821e9c25c6c
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-11-12 11:56:10 -0300

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,40 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If the number string before the separator is not a real number this panic will handle it.
-    if !encoded_value.chars().next().unwrap().is_digit(10) {
-        eprintln!("Unhandled encoded value: {}", encoded_value);
-        std::process::exit(1);
+    if encoded_value.contains(":") {
+        // If the number string before the separator is not a real number this panic will handle it.
+        if !encoded_value.chars().next().unwrap().is_digit(10) {
+            eprintln!("Unhandled encoded value: {}", encoded_value);
+            std::process::exit(1);
+        }
+
+        // Retrieve the index for the separator ":"
+        let colon_index = encoded_value.find(':').unwrap();
+
+        // Get the string before the separator ":"
+        let number_string = &encoded_value[..colon_index];
+
+        // Parse the number string into a i64
+        let number = number_string.parse::<i64>().unwrap();
+
+        // Retrieve the string based on the index of separator, here we trust that the input is the
+        // correctly length (`number`) of the string (`string).
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+
+        // Return the string value
+        return serde_json::Value::String(string.to_string());
+    } else if encoded_value.contains("i") {
+        // Get the number inside the `i` and `e` (i.e. i52e)
+        let number_string = &encoded_value[1..&encoded_value.len() - 1];
+
+        // Parse the string number into a real number
+        let number = number_string.parse::<i64>().unwrap();
+
+        // Return the number value
+        return serde_json::Value::Number(number.into());
+    } else {
+        todo!("we don't know how to handle {encoded_value}");
     }
-
-    // Retrieve the index for the separator ":"
-    let colon_index = encoded_value.find(':').unwrap();
-
-    // Get the string before the separator ":"
-    let number_string = &encoded_value[..colon_index];
-
-    // Parse the number string into a i64
-    let number = number_string.parse::<i64>().unwrap();
-
-    // Retrieve the string based on the index of separator, here we trust that the input is the
-    // correctly length (`number`) of the string (`string).
-    let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-
-    // Return the string value
-    return serde_json::Value::String(string.to_string());
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"

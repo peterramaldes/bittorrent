695557980d2465ebf6d15b5716993d61db6910c7
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-11-12 11:38:22 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,26 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
+    // If the number string before the separator is not a real number this panic will handle it.
+    if !encoded_value.chars().next().unwrap().is_digit(10) {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
+
+    // Retrieve the index for the separator ":"
+    let colon_index = encoded_value.find(':').unwrap();
+
+    // Get the string before the separator ":"
+    let number_string = &encoded_value[..colon_index];
+
+    // Parse the number string into a i64
+    let number = number_string.parse::<i64>().unwrap();
+
+    // Retrieve the string based on the index of separator, here we trust that the input is the
+    // correctly length (`number`) of the string (`string).
+    let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+
+    // Return the string value
+    return serde_json::Value::String(string.to_string());
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -25,13 +34,9 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
